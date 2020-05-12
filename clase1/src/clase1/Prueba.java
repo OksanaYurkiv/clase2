8d9c8e28949146f0232e826e124711cb870f6e69
OksanaYurkiv <oksana.yurkiv.1991@gmail.com>
2020-05-12 20:55:40 +0200

    creando clase

---
+++ clase1/src/clase1/Prueba.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package clase1;
+
+/**
+ * @author oksana
+ *
+ */
+public class Prueba {
+
+	/**
+	 * 
+	 */
+	public Prueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
