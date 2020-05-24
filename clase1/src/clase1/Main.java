1032edc94c10e294bfaf3b94d527adee8bc79674
OksanaYurkiv <oksana.yurkiv.1991@gmail.com>
2020-05-24 21:14:41 +0200

    quito la clase Prueba y añado clase Main

--- clase1/src/clase1/Main.java
+++ clase1/src/clase1/Main.java
@@ -1,24 +1,11 @@
-/**
- * 
- */
 package clase1;
 
-/**
- * @author oksana
- *
- */
-public class Prueba {
+public class Main {
 
-	/**
-	 * 
-	 */
-	public Prueba() {
+	public Main() {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
